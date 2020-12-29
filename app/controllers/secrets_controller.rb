13f454af93ceb76ec727743303b39cf3427f08bf
ryan m <contact@ryanmeek.com>
2020-12-29 14:11:25 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,4 +1,15 @@
 class SecretsController < ApplicationController
-  def new
+
+  before_action :require_login
+ 
+  def show
+
   end
+ 
+  private
+ 
+  def require_login
+    return redirect_to('/') unless session.include? :name
+  end  
+
 end
