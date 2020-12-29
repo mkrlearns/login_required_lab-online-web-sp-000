974a9854a2f8129183cffc7a6d43afced4416211
ryan m <contact@ryanmeek.com>
2020-12-29 14:12:11 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to controller: 'sessions', action: 'new'
+    else
+      session[:name] = params[:name]
+      redirect_to controller: 'sessions', action: 'welcome'
+    end
+  end
+
+  def destroy
+    if !session[:name].nil?
+      session.delete :name  
+    end
+  end
 end
